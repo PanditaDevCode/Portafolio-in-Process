65549d64a84886d54749f1826814c67803b84bac
Alex Moncada <alexander.redes2002@gmail.com>
2024-09-02 22:16:21 -0500

    Add files via upload

---
+++ Personal Web/html/assets/js/script.js
@@ -0,0 +1,56 @@
+// Puntero
+
+document.addEventListener('mousemove', (event) => {
+    const light = document.querySelector('.light');
+    light.style.left = `${event.clientX - light.offsetWidth / 2}px`;
+    light.style.top = `${event.clientY - light.offsetHeight / 2}px`;
+});
+
+// Blur Navbar
+
+document.addEventListener('scroll', () => {
+    const header = document.querySelector('.header');
+
+    if (window.scrollY > 50) { // Ajusta este valor según tus necesidades
+        header.classList.add('blurred');
+    } else {
+        header.classList.remove('blurred');
+    }
+});
+
+
+// Zoom in-out 
+
+const navItems = document.querySelectorAll('.navmenu');
+
+navItems.forEach(item => {
+    item.addEventListener('mouseover', () => {
+        item.classList.add('zoom');
+        item.classList.remove('no-zoom');
+    });
+
+    item.addEventListener('mouseout', () => {
+        item.classList.remove('zoom');
+        item.classList.add('no-zoom');
+    });
+});
+
+
+const button = document.getElementById('zoomButton');
+
+button.addEventListener('mouseover', () => {
+    button.classList.add('zoom');
+    button.classList.remove('no-zoom');
+});
+
+button.addEventListener('mouseout', () => {
+    button.classList.remove('zoom');
+    button.classList.add('no-zoom');
+});
+
+
+
+window.addEventListener('scroll', function () {
+    var header = document.querySelector('header');
+    header.classList.toggle('abajo', window.scrollY > 0);
+});
